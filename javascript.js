b0a8fe5eddd1700c926cd8bcccbf83ec8ab210a7
Hannah <hannahxchapman@yahoo.com>
2022-04-14 11:48:14 -0700

    Save

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 //set the number of rows and columns
-let gridNumber = 64;
+let gridNumber = 16;
 let numOfSquares = gridNumber * gridNumber;
 //for each gridNumber, create gridNumber x gridNumber div squares
 
@@ -7,7 +7,7 @@ for (i=0; i<numOfSquares; i++) {
     const container = document.querySelector('#container');
     const div = document.createElement('div');
     div.classList.add('square');
-    container.appendChild(square);
+    container.appendChild(div);
 }
 //and then style the container as a grid with gridNumber rows and gridNumber columns
 //if reset button is pressed, prompt for # of squares and return as gridNumber
