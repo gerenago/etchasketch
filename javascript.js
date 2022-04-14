d07e241da2db2762372a3a488a7dea4879cdd4f1
Hannah <hannahxchapman@yahoo.com>
2022-04-14 15:52:45 -0700

    Fix changing squares effect

--- javascript.js
+++ javascript.js
@@ -3,19 +3,42 @@ let gridNumber = 16;
 let numOfSquares = gridNumber * gridNumber;
 //for each gridNumber, create gridNumber x gridNumber div squares
 
-for (i=0; i<numOfSquares; i++) {
-    const container = document.querySelector('#container');
-    const div = document.createElement('div');
-    div.classList.add('square');
-    container.appendChild(div);
+function addClass () {
+    for (i=0; i<numOfSquares; i++) {
+        const container = document.querySelector('#container');
+        const div = document.createElement('div');
+        div.classList.add('square');
+        container.appendChild(div);
+    }
 }
 //and then style the container as a grid with gridNumber rows and gridNumber columns
-const container = document.querySelector('#container');
-container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+function createGrid () {
+    const container = document.querySelector('#container');
+    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+}
 
 //when mouse hovers over square, change color permanently to black
-const square = document.querySelectorAll('.square');
-square.forEach(square => square.addEventListener("mouseover", function (e) {
-    square.style.backgroundColor = "black";
-}))
+function drawEffect () {
+    const square = document.querySelectorAll('.square');
+    square.forEach(square => square.addEventListener("mouseover", function (e) {
+        square.style.backgroundColor = "black";
+    }))
+}
 //if reset button is pressed, prompt for # of squares and return as gridNumber
+
+const btn = document.querySelector('.reset');
+btn.addEventListener("click", function (e) {
+    const square = document.querySelectorAll('.square');
+    square.forEach(e => e.remove());
+    let input = window.prompt("Number of squares per side for new grid");
+    gridNumber = parseInt(input)
+    numOfSquares = gridNumber * gridNumber;
+    addClass(numOfSquares)
+    createGrid(gridNumber)
+    drawEffect()
+
+
+})
+addClass()
+createGrid()
+drawEffect()
